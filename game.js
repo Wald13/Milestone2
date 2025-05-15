0914fc911a926b76cba62abff07403b678945588
Wilson <waldmendes@live.com>
2025-05-15 15:08:48 +0100

    Adjust styles to the navbar link buttons

--- game.js
+++ game.js
@@ -4,3 +4,19 @@ document.querySelectorAll('.card').forEach(card => {
         inner.classList.toggle('flipped');
     });
 });
+// Add event listener to instructions button to open modal
+document.getElementById("openInstructions").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("instructionsModal").style.display = "block";
+});
+
+document.querySelector("#instructionsModal .close").addEventListener("click", function () {
+  document.getElementById("instructionsModal").style.display = "none";
+});
+
+window.addEventListener("click", function (event) {
+  const modal = document.getElementById("instructionsModal");
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+});
